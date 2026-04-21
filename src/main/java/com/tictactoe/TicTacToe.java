5a3e42ab7db140d5cdc969ad87b7bbf977361793
SWETHA-221107 <swetham3695@gmail.com>
2026-04-21 15:32:17 +0530

    UC2:toss to decide first player and symbol

--- src/main/java/com/tictactoe/TicTacToe.java
+++ src/main/java/com/tictactoe/TicTacToe.java
@@ -1,31 +1,41 @@
 package com.tictactoe;
 
+import java.util.Random;
+
 public class TicTacToe {
 
-    // Function to initialize the board
-    public static void initializeBoard(char[][] board) {
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                board[i][j] = '-';
-            }
-        }
-    }
+    // Game state variables
+    static String currentPlayer;
+    static char player1Symbol;
+    static char player2Symbol;
 
-    // Function to display the board
-    public static void displayBoard(char[][] board) {
-        System.out.println("\nTic-Tac-Toe Board:\n");
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                System.out.print(board[i][j] + " ");
-            }
-            System.out.println();
+    // Function to perform toss and assign symbols
+    public static void tossAndAssign() {
+        Random random = new Random();
+
+        int toss = random.nextInt(2); // 0 or 1
+
+        if (toss == 0) {
+            currentPlayer = "Player 1";
+            player1Symbol = 'X';
+            player2Symbol = 'O';
+        } else {
+            currentPlayer = "Player 2";
+            player1Symbol = 'O';
+            player2Symbol = 'X';
         }
+
+        // Display results
+        System.out.println("Toss Result: " + toss);
+        System.out.println(currentPlayer + " will start first!");
+        System.out.println("Player 1 Symbol: " + player1Symbol);
+        System.out.println("Player 2 Symbol: " + player2Symbol);
     }
 
     public static void main(String[] args) {
-        char[][] board = new char[3][3]; // 2D array
+        System.out.println("Starting Tic-Tac-Toe Game...\n");
 
-        initializeBoard(board); // Initialize with '-'
-        displayBoard(board);    // Print board
+        tossAndAssign(); // Perform UC2
     }
 }
+
